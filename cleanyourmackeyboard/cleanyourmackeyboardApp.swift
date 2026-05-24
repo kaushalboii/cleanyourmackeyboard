01c90c2c5d8be8bf1ac528bf4394b633c8808f07
kaushalboii <kaushalrr89@gmail.com>
2026-05-25 00:28:11 +0530

    Initial Commit

---
+++ cleanyourmackeyboard/cleanyourmackeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  cleanyourmackeyboardApp.swift
+//  cleanyourmackeyboard
+//
+//  Created by Vishal rao on 25/05/26.
+//
+
+import SwiftUI
+
+@main
+struct cleanyourmackeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
